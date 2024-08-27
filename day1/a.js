f879341b53a99d99e3b2b2760f5470a7b946bfe3
kaushal8582 <kaushal02917@gmail.com>
2024-08-27 20:09:18 +0530

    web tech

---
+++ day1/a.js
@@ -0,0 +1,62 @@
+// let 
+// var
+// const 
+
+// const a=2.2334;
+
+// a=344
+
+
+// let a =23;
+// a =34553
+
+// var b =234;
+// b=3454656765;
+
+// {
+//  var a =23;
+//  let k =34;
+// }
+
+// console.log(a);
+// console.log(k);
+
+//  truty and falsy
+// true  - false
+// 0 -- 1-infi
+
+// if(undefined){
+//     console.log("kaushla");
+// }
+
+
+// let a ="kaushal";
+// var b =34;
+// const c =12;
+
+// if(a>b){
+//     console.log(a);
+// }else if(0){
+//     console.log(b);
+// }else{
+//     console.log(c);
+// }
+
+
+
+switch (4) {
+    case 1:
+        console.log("press 1");
+        break;
+    case 2:
+        console.log("press 2");
+        break;
+    default:
+        // console.log("nothing");
+        break;
+}
+
+
+// variable
+// truty falsy
+// condition- if elseif else,, switch
---
+++ day2/day2.js
@@ -0,0 +1,102 @@
+// ternary operator
+
+// condition?ex1:ex2
+
+// let a = 12>14?12:14
+
+// console.log(a);
+
+
+// if(true){
+    /// code
+// }
+
+
+// let marks =20
+// let c = (marks==10)?'yes':'no';
+// console.log(c);
+
+// let obj  = {
+//     name :"kaushal",
+//     college:"RDS",
+// }
+
+// console.log(obj.name);
+// console.log(obj.college);
+
+// let  kuchBhi = {
+//     name:"kaushal",
+//     college:"RDS",
+//     roll:"09",
+// }
+
+// console.log(kuchBhi.college);
+// console.log(kuchBhi);
+
+
+// const fruits =["apple", "banana","apes","grapes"];
+
+// const a =[1,"kaushal",false,123.23];
+
+// console.log(a[1]);
+
+// console.log(a[1]); // 1
+// console.log(fruits[1]); // apple 
+
+// a[1] = "Beauty";
+
+// console.log(a[1]);
+
+
+
+const a  = [7,9,3,4,5,6];
+
+
+// method
+// console.log(a.length);
+// console.log(a.push(7));
+// console.log(a);
+// console.log(a.pop());
+// console.log(a);
+
+// console.log(a.toString());
+
+// console.log(a.join("+1,"));
+
+// let b =[6,7,8,9,10];
+
+// const c=(a.concat(b));
+// console.log(c);
+
+
+// console.log(a.sort());
+
+
+// LOOPS
+
+// for loop
+
+// for(init ; condition; inc or dec){
+
+// }
+
+for(let i =0; i<=10; i++){
+    console.log("kaushal");
+}
+
+// while
+//do while
+// for in
+// for of 
+// for each
+
+
+
+
+
+
+
+
+ 
+
+
