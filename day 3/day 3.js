1df897a3ae39308c7affd99b407b837933ec75ee
kaushal8582 <kaushal02917@gmail.com>
2024-08-28 21:59:33 +0530

    day 4

--- day 3/day 3.js
+++ day 3/day 3.js
@@ -56,4 +56,3 @@
 
 // console.log(1+'3');
 // console.log(1-'3');
-
---
+++ day 4/day4.js
@@ -0,0 +1,92 @@
+// Write a JavaScript program that calculates the sum of all numbers from 1 to a given number n.
+//  For example, if n is 10, the output should be 55. Use a loop to calculate the sum
+
+// let n =15;
+
+// let sum =0;
+
+// for(let i=1;i<=n;i++){
+//     // sum+=i;
+//     sum =sum+i;
+// }
+
+// console.log(sum);
+
+
+// Create a JavaScript program that prints a pattern like the following using a 
+// nested loop:
+// *
+// **
+// ***
+// ****
+// *****
+
+
+
+// for (let i = 0; i <= 10; i++) {
+//     let stars ="";
+//     for (let j = 0; j <= i; j++) {
+//         stars = stars + "*";
+//     }
+//     console.log(stars);
+// }
+
+
+//              String
+
+
+// let name ='khushi';
+// let name2 ="kau";
+
+// let name3 = `kaushal${name2}`;
+
+
+// let word = `the name' is "kaushal" `
+
+let clg = "RDS College MUZ."
+
+// console.log(clg.length);
+// console.log(clg.toLowerCase());
+// console.log(clg.toUpperCase());
+// let newClg = clg.replace("RDS","LS");
+// console.log(newClg);
+// console.log(clg.slice(4,12));
+// console.log(clg.slice(4));
+// console.log(clg.slice(0,2));
+// console.log(clg.charAt(9));
+// console.log(clg.includes(" ."));
+
+// let arr =[1,2,3,4,5]
+
+let name1 =  "     kausahl     ";
+// console.log(name1.length);
+// console.log(name1);
+// console.log(name1.trim());
+
+let k = "Nikita  Kumari";
+console.log(k);
+
+console.log(k.split("Nikita  Kumari"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
