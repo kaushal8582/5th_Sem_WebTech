54d48bb890553f4200a97d66fc7ad7ca6b341980
kaushal8582 <kaushal02917@gmail.com>
2024-09-06 21:32:06 +0530

    web dev first class

---
+++ day 11/index.js
@@ -0,0 +1,14 @@
+// functilonaly provide karna 
+
+// console.log(window.document);
+// console.dir(window.document);
+
+// window.console.log("Kaushal")
+// console.log("kaushal");
+
+// window -> document -> html -> head
+                            // -> body
+
+
+
+
