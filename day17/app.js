e6f3dfdd281d4dd8c4113254558c3cf0ea75fc0d
kaushal8582 <kaushal02917@gmail.com>
2024-09-14 21:36:38 +0530

    update 17 repeat

--- day17/app.js
+++ day17/app.js
@@ -2,22 +2,24 @@
 
 // const inp = document.querySelector("input")
 
-// inp.addEventListener("keydown",function(e){
-//     // console.log("presed");
-//     // console.log( "code : ", e.code);
-//     // console.log("key : ",e.key);
-//     // console.log("key code : ", e.keyCode);
 
-//     if(e.code=="ArrowUp"){
-//         console.log("moved upper");
-//     }else if(e.code=="ArrowDown"){
-//         console.log('moved down');
-//     }else if(e.code=="ArrowLeft"){
-//         console.log("moved left");
-//     }else if(e.code=="ArrowRight"){
-//         console.log("moved right");
-//     }
 
+// inp.addEventListener("keyup",function(e){
+//     console.log(e);
+    
+    // console.log( "code : ", e.code);
+    // console.log("key : ",e.key);
+    // console.log("key code : ", e.keyCode);
+
+    // if(e.code=="ArrowUp"){
+    //     console.log("moved upper");
+    // }else if(e.code=="ArrowDown"){
+    //     console.log('moved down');
+    // }else if(e.code=="ArrowLeft"){
+    //     console.log("moved left");
+    // }else if(e.code=="ArrowRight"){
+    //     console.log("moved right");
+    // }
 
 // })
 
@@ -27,27 +29,34 @@
 // })
 
 
+// form event
+
 const form = document.querySelector("form");
-// const inp = document.querySelector("input")
-// const pass = document.querySelector("#pass")
+const inp = document.querySelector("input")
+const pass = document.querySelector("#pass")
+
+const emailH1 = document.querySelector("h1 span")
+const passwordH2 = document.querySelector("h2 span")
+
 
 form.addEventListener("submit",function(e){
     e.preventDefault()
-//    console.log(inp.value);
-//    console.log(pass.value);
+
+    let emailValue = inp.value;
+    let passwordValue = pass.value;
+
+   emailH1.innerHTML = emailValue;
+   passwordH2.innerHTML = passwordValue
 
     // console.log(form.elements[0].value);
     // console.log(form.elements[1].value);
-    console.log(this.elements[0].value);
-    console.log(this.elements[1].value);
-    
-    
-
+    // console.log(this.elements[0].value);
+    // console.log(this.elements[1].value);
 })
 
-"name: kaushal"
-"email : kau@g.com"
-"roll: 12"
+// "name: kaushal"
+// "email : kau@g.com"
+// "roll: 12"
 
 
 
