55faee79a313d2287856d1ff4276f78f71497e38
kaushal <kaushalkumar02918@gmail.com>
2024-10-17 21:09:33 +0530

    oops

--- OOPS/index.js
+++ OOPS/index.js
@@ -81,33 +81,33 @@
 //  Inheritance
 
 
-class Parent{
-    constructor(name,age){
-        this.name = name;
-        this.age = age;
-    }
-    getParentDetails(){
-        console.log(this.name);
-        console.log(this.age);
-    }
-}
+// class Parent{
+//     constructor(name,age){
+//         this.name = name;
+//         this.age = age;
+//     }
+//     getParentDetails(){
+//         console.log(this.name);
+//         console.log(this.age);
+//     }
+// }
 
 
 
-class Child extends Parent {
+// class Child extends Parent {
 
-    constructor(name,age,roll){
-        super(name,age)
-        this.roll =roll
+//     constructor(name,age,roll){
+//         super(name,age)
+//         this.roll =roll
         
-    }
+//     }
 
 
-    print(){
-        console.log("This is Child");
-        super.getParentDetails()
-    }
-}
+//     print(){
+//         console.log("This is Child");
+//         super.getParentDetails()
+//     }
+// }
 
 // let c1 = new Child("asdf",99,23);
 // c1.getParentDetails()
@@ -116,6 +116,106 @@ class Child extends Parent {
 
 
 
+// class Calculate{
+
+//     multiply(a,b){
+//         return a*b;
+//     }
+
+// }
 
 
 
+// class Sum extends Calculate {
+//     sum(a,b){
+//         return a+b;
+//     }
+
+//     multiply(a,b){
+//       let ans =  super.multiply(a,b)
+//        console.log("This is child method");
+//        return ans
+//     }
+// }
+
+
+// let obj1 = new Sum();
+
+
+
+// console.log( "sum of two number : ", obj1.sum(8,9));
+// console.log( "mul of two number : ", obj1.multiply(8,9));
+
+
+
+// ProtoType
+
+
+// let obj ={
+//     a:"23",
+//     b:24
+// };
+
+
+// obj.__proto___ = function name(){
+//     console.log("this is name");
+// }
+
+
+// get and set
+
+// class Car {
+//     constructor(name) {
+//       this._name = name;
+//     }
+  
+//     get name() {
+//       return this._name;
+//     }
+  
+//     set name(newName) {
+//       this._name = newName;
+//     }
+
+//     printData(){
+//         console.log(this._name);
+//     }
+
+//     setName(newName){
+//         this._name = newName;
+//     }
+
+//   }
+  
+//   const car1 = new Car('BMW');
+//   console.log(car1.name); // Output: BMW
+//   car1.name = 'Audi';
+//   console.log(car1.name); // Output: Audi
+
+
+
+// car1.printData();
+// car1.printData();
+// car1.setName("Maruti")
+
+
+
+// static 
+
+class Car {
+ 
+  static printSome(){
+    console.log("this is oops classes");
+  }
+
+   printSome2(){
+    console.log("this is oops ");
+  }
+
+}
+
+let car1 = new Car()
+
+car1.printSome2();
+
+Car.printSome()
